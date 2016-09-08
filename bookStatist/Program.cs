7396c0ecb18baaa00c3c3e34409c49203e6897f2
cyrillx <e@mail.org>
2016-09-08 21:50:09 +0300

    Добавлено сохранение в файл

--- bookStatist/Program.cs
+++ bookStatist/Program.cs
@@ -8,38 +8,36 @@ using System.Text.RegularExpressions;
 * bookStat список слов wordStat
 */
 
-namespace bookStatist
-{
-    class Program
-    {
-        static void Main(string[] args)
-        {
+namespace bookStatist {
+    class Program {
+        static void Main(string[] args) {
+            String src, dst;
             if (args.Length == 0) return;
+            src = args[0];
 
             // создать объект bookStat
             bookStat bs = new bookStat();
             bs.load(args[0]);
 
             bs.print();
+
+            if (args.Length < 2) return;
+            dst = args[1];
+            bs.save(dst);
         }
 
     }
 
-    class bookStat
-    {
+    class bookStat {
         List<wordStat> words;
-        public bookStat()
-        {
+        public bookStat() {
             words = new List<wordStat>();
         }
 
-        public void count(String s)
-        {
+        public void count(String s) {
             s = s.ToLower();
-            foreach (wordStat ws in words)
-            {
-                if (ws.isIn(s))
-                {
+            foreach (wordStat ws in words) {
+                if (ws.isIn(s)) {
                     ws.count();
                     return;
                 }
@@ -49,25 +47,20 @@ namespace bookStatist
             words.Add(newws);
         }
 
-        public void print()
-        {
-            foreach (wordStat ws in words)
-            {
+        public void print() {
+            foreach (wordStat ws in words) {
                 String s = String.Format("{0}: {1}", ws.word, ws.entries);
                 Console.WriteLine(s);
             }
         }
 
-        public bool load(string src)
-        {
+        public bool load(string src) {
             // открыть текст
             StreamReader streamReader;
-            try
-            {
+            try {
                 streamReader = new StreamReader(src);
             }
-            catch
-            {
+            catch {
                 Console.WriteLine("File does not exist!");
                 return false;
             }
@@ -79,38 +72,50 @@ namespace bookStatist
             List<string> ls = new List<string>();
             Array ar = Array.CreateInstance(typeof(Match), mc.Count);
             mc.CopyTo(ar, 0);
-            foreach (Match m in mc)
-            {
+            foreach (Match m in mc) {
                 ls.Add(m.Value);
             }
             ls.Sort();
 
-            for (int i = 0; i < ls.Count; i++)
-            {
+            for (int i = 0; i < ls.Count; i++) {
                 count(ls[i].ToString());
             }
 
             return true;
         }
+
+        public bool save(string dst) {
+            StreamWriter streamWriter;
+            try {
+                streamWriter = new StreamWriter(dst);
+            }
+            catch {
+                return false;
+            }
+
+            foreach (wordStat ws in words) {
+                string s = String.Format("{0} {1}", ws.word, ws.entries);
+                streamWriter.WriteLine(s);
+            }
+            streamWriter.Close();
+
+            return true;
+        }
     }
 
-    public class wordStat
-    {
+    public class wordStat {
         public String word;
         public uint entries;
-        public wordStat(string s, uint e = 0)
-        {
+        public wordStat(string s, uint e = 0) {
             word = s;
             entries = e;
         }
 
-        public bool isIn(String s)
-        {
+        public bool isIn(String s) {
             return s == word;
         }
 
-        public void count()
-        {
+        public void count() {
             entries++;
         }
     }
