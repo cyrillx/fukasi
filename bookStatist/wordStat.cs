df8ca5dc6db4bc96b3173ab90ad465e1bab805f6
Kirill <cyrillx@users.noreply.github.com>
2016-09-25 16:26:42 +0300

    Классы разнесены по отдельным файлам

---
+++ bookStatist/wordStat.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace bookStatist {
+    public class wordStat {
+        public String word;
+        public uint entries;
+        public wordStat(string s, uint e = 0) {
+            word = s;
+            entries = e;
+        }
+
+        public bool isIn(String s) {
+            return s == word;
+        }
+
+        public void count(uint a = 1) {
+            entries += a;
+        }
+
+    }
+}
